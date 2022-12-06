15f88a72d4a28aea857ab84caa45aa1651aca82e
stefanlilja <stefanlilja@brightstraining.com>
2022-12-06 10:40:41 +0100

    added fetch-and-print function

--- assess1.py
+++ assess1.py
@@ -1,10 +1,24 @@
 import psycopg2
 
 def get_database_connection():
-    return psycopg2.connection(
+    return psycopg2.connect(
         host='localhost',
         database='assessment1',
         user='postgres',
         password='continuousimpliesintegrable'
     )
 
+def read_from_database():
+    connection = get_database_connection()
+    cur=connection.cursor()
+    cur.execute('SELECT * FROM view_contacts;')
+    rows = cur.fetchall()
+    cur.close()
+    connection.close()
+    for item in rows:
+        print(item)
+
+def main():
+    read_from_database()
+
+main()
